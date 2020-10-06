d546fc8ba91f7cca8a0419bdba1170957a91c174
Matias Klemola <matias.klemola@gmail.com>
2020-10-06 11:11:07 +0300

    Add content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+  <Layout title="Brewery">
+    <div className="beer">{"🍺".repeat(99)}</div>
   </Layout>
 )
 
